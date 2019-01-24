6dffb8be727dcde520a67ee21e495f7c21011ed9
Jin Wang <wangatqueens@gmail.com>
2019-01-23 23:06:03 -0500

    Rest signup done
    can POST user to api.com

--- restful/index.php
+++ restful/index.php
@@ -37,6 +37,11 @@ class Restful {
 		try {
 			$this->_setupRequestMethod();
 			$this->_setupResources();
+			if($this->_resourceName == 'users'){
+				return $this->_json($this->_handleUser());
+			}else{
+				return $this->_handleArticle();
+			}
 		} catch (Exception $e) {
 			$this->_json(['error'=>$e->getMessage()],$e->getCode());
 		}
@@ -53,20 +58,19 @@ class Restful {
 	// initialize request resources
 	private function _setupResources(){
 		$path = $_SERVER['PATH_INFO'];
-		//echo $path;
+		//echo "path is".$path;
 		$params = explode('/',$path);
 		//print_r($params);
 		//Array
 		// (
 		//     [0] => 
 		//     [1] => articles
-		//     [2] => 123
+		//     [2] => 
 		// )
 		$this->_resourceName = $params[1]; // name of resources in $params[1]
-		if(!in_array($this->_resourceName, $this->_allowResources)){
-			throw new Exception('Request Resouces Not Allowed', 403);
-			
-		}
+		// if(!in_array($this->_resourceName, $this->_allowResources)){
+		// 	throw new Exception('Request Resouces Not Allowed', 403);
+		// }
 		if(!empty($params[2])){
 			$this->_id = $params[2];
 		}
@@ -80,6 +84,35 @@ class Restful {
 		header('Content-Type:application/json;charset=utf-8');
 		echo json_encode($array, JSON_UNESCAPED_UNICODE);
 		exit(); 
+	}
+	// Request for user resources
+	private function _handleUser(){
+		if($this->_requestMethod != 'POST'){
+			throw new Exception('Request method is not allowed', 405);
+		}
+		$body = $this->_getBodyParams();
+
+		//var_dump($body);
+		if(empty($body['username'])){
+			throw new Exception('Username Cannot be Empty', 400);	
+		}
+		if(empty($body['password'])){
+			throw new Exception('Password Cannot be Empty', 400);
+		}
+		return  $this->_user->signup($body['username'],$body['password']);
+		
+	}
+	// get request body
+	private function _getBodyParams(){
+		$raw = file_get_contents('php://input');
+		if(empty($raw)){
+			throw new Exception('Request Params Error', 400);
+		}
+		return json_decode($raw,true);  
+	}
+	// Request for Articles resources
+	private function _handleArticle(){
+
 	}
 }
 
